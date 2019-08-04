3d328354ea25971c25a4efa3ec224400850e3e13
meshde <meshde.md@gmail.com>
2019-08-04 17:31:25 +0530

    Fix: Don't send shell output if socket not open

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ expressWs.app.ws('/shell', (ws, req) => {
   });
   // For all shell data send it to the websocket
   shell.on('data', (data) => {
-    ws.send(data);
+    if (ws.readyState === 1) {
+      ws.send(data);
+    }
   });
   // For all websocket data send it to the shell
   ws.on('message', (msg) => {
