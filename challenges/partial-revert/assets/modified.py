3f45684d69df5c96240f9cbb867db3ce6909e6c8
meshde <meshde.md@gmail.com>
2020-01-13 12:20:48 +0530

    Add challenge: partial-revert

---
+++ challenges/partial-revert/assets/modified.py
@@ -0,0 +1,26 @@
+def function1(x):
+    # Changes to be reverted
+    if not x:
+        return
+    # Complicated Logic
+    # spanning
+    # 100 lines
+    return
+
+def function2(x):
+    # Changes to be kept
+    if not x:
+        return
+    # Complicated Logic
+    # spanning
+    # 100 lines
+    return
+
+def function3(x):
+    # Changes to be reverted
+    if not x:
+        return
+    # Complicated Logic
+    # spanning
+    # 100 lines
+    return
---
+++ challenges/partial-revert/assets/original.py
@@ -0,0 +1,17 @@
+def function1():
+    # Complicated Logic
+    # spanning
+    # 100 lines
+    return
+
+def function2():
+    # Complicated Logic
+    # spanning
+    # 100 lines
+    return
+
+def function3():
+    # Complicated Logic
+    # spanning
+    # 100 lines
+    return
